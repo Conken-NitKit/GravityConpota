05465f7329020233891cadbe1de77cf7cb4f8dcb
Gajikimaguro <83415763+Kajikiryoma@users.noreply.github.com>
2021-11-09 18:22:30 +0900

    changebranch

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody2D;
+    float speed;
+    public enum MOVE_DIRECTION
+    {
+        STOP,
+        LEFT,
+        RIGHT,
+    }
+    MOVE_DIRECTION moveDirection = MOVE_DIRECTION.STOP;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        if (x == 0)
+        {
+            // Ž~‚Ü‚é
+            moveDirection = MOVE_DIRECTION.STOP;
+        }
+        else if (x > 0)
+        {
+            // ‰E‚ÉˆÚ“®
+            moveDirection = MOVE_DIRECTION.RIGHT;
+        }
+        else if (x < 0)
+        {
+            // ¶‚ÉˆÚ“®
+            moveDirection = MOVE_DIRECTION.LEFT;
+        }
+    }
+    private void FixedUpdate()
+    {
+        switch (moveDirection)
+        {
+            case MOVE_DIRECTION.STOP:
+                speed = 0;
+                break;
+            case MOVE_DIRECTION.LEFT:
+                speed = -3;
+                break;
+            case MOVE_DIRECTION.RIGHT:
+                speed = 3;
+                break;
+        }
+        rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+    }
+}
